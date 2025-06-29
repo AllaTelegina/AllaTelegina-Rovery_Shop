88195fb7c16f217b9c53e8d67882745429ec35af
Dogmax104 <maxpesfix@gmail.com>
2025-06-29 15:21:25 +0200

    29.06.2025

---
+++ Models/UserRovery.cs
@@ -0,0 +1,18 @@
+﻿namespace Backend_asp.net.Models
+{
+    public class UserRovery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRovery() { }
+        public UserRovery(int id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
