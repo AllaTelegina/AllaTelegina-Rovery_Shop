ba641a3511da10090fdf2fbefbd0381596210add
AllaTelegina <allatelegina9@gmail.com>
2025-07-13 23:24:35 +0300

    forgot_password

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return View("forgot_password");
     }
 
     public IActionResult Privacy()
