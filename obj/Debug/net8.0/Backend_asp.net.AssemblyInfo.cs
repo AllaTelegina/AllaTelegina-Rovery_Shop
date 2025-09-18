6fd027e15f19fe45fd175d3d3a0d9170fe098e8c
Dogmax104 <maxpesfix@gmail.com>
2025-09-18 22:40:02 +0200

    no save it is commit

--- obj/Debug/net8.0/Backend_asp.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_asp.net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d883b71b50b192e97d9f4ae7a02b29b167d227")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05567e8507bbd585800acc598c23986fe58fdcba")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
