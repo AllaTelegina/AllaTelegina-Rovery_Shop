b03644d6e6f5d90489aadc114226f6e994f42408
Dogmax104 <maxpesfix@gmail.com>
2026-03-08 23:02:23 +0100

    commit branch Archeticture_database1 08.03.2026 23:02

--- obj/Debug/net8.0/Backend_asp.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_asp.net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577fbcc6fa26fb40f54d5320a8caa892853b88fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c51a5d23c3e97d0e5cdc6173d650fab6009cee")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerProjectBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
